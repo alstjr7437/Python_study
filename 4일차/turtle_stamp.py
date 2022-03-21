e0994f035f4709493ef43e223a739cb6e92dd049
김민석|Minseok Kim <alstjr7437@naver.com>
2022-03-21 10:40:10 +0900

    스탬프 수정

--- 4일차/turtle_stamp.py
+++ 4일차/turtle_stamp.py
@@ -1,37 +1,41 @@
 import turtle
 import random
 
-##함수 선언 부분##
+##함수 부분
+# 왼쪽 마우스버튼 클릭하여 도장찍기
 def screenLeftClick(x, y):
-    global r, g, b
-    turtle.pencolor((r, g, b))
-    turtle.pendown()
-    turtle.goto(x, y)
-
-def screenRightClick(x, y):
-    turtle.penup()
-    turtle.goto(x, y)
-
-def screenMidClick(x, y):
-    global r, g, b
-    tSize = random.randrange(1, 10)
-    turtle.shapesize(tSize)
+    global r,g,b
     r = random.random()
     g = random.random()
     b = random.random()
+    
+#크기는 2부터 10
+    tSize = random.randrange(2,10)
+#각도는 0부터 360도 
+    tAngle = random.randrange(0, 360)
+#크기 색상 각도
+    turtle.shapesize(tSize)
+    turtle.color(r,g,b)
+    turtle.right(tAngle)
+#거북이 도장 찍기
+    turtle.stamp()
 
-##변수 선언 부분##
-pSize, tSize = 10, 0
-r, g, b = 0.0, 0.0, 0.0
+#거북이 이동 
+def screenRightClick(x,y):
+    turtle.penup()
+    turtle.goto(x,y)
 
-##메인 코드 부분##
-turtle.title('거북이로 그림 그리기')
-turtle.shape('turtle')
-turtle.pensize(pSize)
+##변수 부분
+#변수 초기값
+r,g,b = 0.0,0.0,0.0
 
+##메인 부분
+turtle.title("거북이 도장 찍기")
+turtle.shape("turtle")
+
+#함수 호출
 turtle.onscreenclick(screenLeftClick, 1)
-turtle.onscreenclick(screenMidClick, 2)
-turtle.onscreenclick(screenRightClick, 3)
+turtle.onscreenclick(screenRightClick,3)
 
 turtle.done()
 
