89dae5a84272d9ffd22b944f5f6933f71a6efc62
김민석|Minseok Kim <alstjr7437@naver.com>
2022-03-21 11:29:19 +0900

    스탬프 수정

--- 4일차/turtle_stamp.py
+++ 4일차/turtle_stamp.py
@@ -4,11 +4,15 @@ import random
 ##함수 부분
 # 왼쪽 마우스버튼 클릭하여 도장찍기
 def screenLeftClick(x, y):
+#색상 랜덤 주기
     global r,g,b
     r = random.random()
     g = random.random()
     b = random.random()
-    
+
+# 거북이 이동
+    turtle.penup()
+    turtle.goto(x,y)
 #크기는 2부터 10
     tSize = random.randrange(2,10)
 #각도는 0부터 360도 
@@ -20,11 +24,6 @@ def screenLeftClick(x, y):
 #거북이 도장 찍기
     turtle.stamp()
 
-#거북이 이동 
-def screenRightClick(x,y):
-    turtle.penup()
-    turtle.goto(x,y)
-
 ##변수 부분
 #변수 초기값
 r,g,b = 0.0,0.0,0.0
@@ -35,7 +34,6 @@ turtle.shape("turtle")
 
 #함수 호출
 turtle.onscreenclick(screenLeftClick, 1)
-turtle.onscreenclick(screenRightClick,3)
 
 turtle.done()
 
