a38c27fb4db21222cf3b6b4f57b572ba42f8806f
김민석|Minseok Kim <alstjr7437@naver.com>
2022-03-28 23:45:03 +0900

    6일차 7일차 수업 코드 커밋

---
+++ 7일차/day7_1.py
@@ -0,0 +1,12 @@
+a = 'happy'
+b = 'love'
+print(f"a = {a} b = {b}"); # f string
+print("a = {} b = {}".format(a,b)); # 위치 인자
+print("a = {k1} b = {k2}".format(k1 = a,k2 = b)); #키워드 인자
+
+s1 = 'aaaaaaaaaaaaaaaaa' \
+    'bbbbbbbbbbbbbbb'
+print(s1)
+
+print("\hello")
+print("I don\t know")
